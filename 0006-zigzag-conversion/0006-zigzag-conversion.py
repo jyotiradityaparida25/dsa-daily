241dbec9a52fc17d4bdcac2fe3f9af21aa84ec12
Jyotiraditya Parida <jyotiradityaparida0026@gmail.com>
2026-07-13 23:42:41 +0530

    Time: 15 ms (45.71%), Space: 12.6 MB (20.89%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        curr_row = 0
+        direction = -1
+
+        for ch in s:
+            rows[curr_row] += ch
+
+            if curr_row == 0 or curr_row == numRows - 1:
+                direction *= -1
+
+            curr_row += direction
+
+        return "".join(rows)
+        
