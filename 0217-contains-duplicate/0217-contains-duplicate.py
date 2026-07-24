a25b630cbd465c6afcccd215aae682ab85fac4b0
Jyotiraditya Parida <jyotiradityaparida0026@gmail.com>
2026-07-24 23:53:54 +0530

    Time: 10 ms (94.17%), Space: 25.9 MB (34.29%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s=set(nums)
+        if len(nums)!=len(s):
+            return True
+        return False
+        
