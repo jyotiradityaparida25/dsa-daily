0215bdbee7300e7f33df9fff96178c42fc50d600
Jyotiraditya Parida <jyotiradityaparida0026@gmail.com>
2026-07-27 00:06:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (29.91%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -0,0 +1,34 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        memo = {}
+        
+        def compute(expr: str) -> list[int]:
+            
+            if expr in memo:
+                return memo[expr]
+            
+            results = []
+            
+            for i, char in enumerate(expr):
+                if char in "+-*":
+               
+                    left_results = compute(expr[:i])
+                    right_results = compute(expr[i+1:])
+                    
+                    
+                    for left in left_results:
+                        for right in right_results:
+                            if char == '+':
+                                results.append(left + right)
+                            elif char == '-':
+                                results.append(left - right)
+                            elif char == '*':
+                                results.append(left * right)
+         
+            if not results:
+                results.append(int(expr))
+                
+            memo[expr] = results
+            return results
+            
+        return compute(expression)
