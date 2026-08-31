5d6884102806b4ac49e67974a5c83f89572fca71
Jyotiraditya Parida <jyotiradityaparida0026@gmail.com>
2026-08-31 10:52:57 +0530

    Time: 60 ms (97.17%), Space: 22 MB (70.51%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m=len(mat)
+        n=len(mat[0])
+        res = [[float('inf')] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]==0:
+                    res[i][j]=0
+                else:
+                    if i > 0:
+                        res[i][j] = min(res[i][j], res[i - 1][j] + 1)
+                 
+                    if j > 0:
+                        res[i][j] = min(res[i][j], res[i][j - 1] + 1)
+                        
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if res[i][j] != 0:
+
+                    if i < m - 1:
+                        res[i][j] = min(res[i][j], res[i + 1][j] + 1)
+
+                    if j < n - 1:
+                        res[i][j] = min(res[i][j], res[i][j + 1] + 1)
+                        
+        return res
